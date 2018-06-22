bea877775312e98ee5bf6da4eac81de72a23754c
hero2bash <zero.bash@gmail.com>
2018-06-22 20:21:53 +0800

    Create temporary global object for values

---
+++ docs/creds.js
@@ -0,0 +1,6 @@
+var purecloudSpotifyCreds = {
+    spotify_clientId : '76a609654bf14ed9aecff16b15110c12',
+    pureCloud_clientId : '8c821827-57bd-4d44-8765-597d4a3220c5',
+    landing_page : 'https://princemerluza.github.io/purecloud-spotify-presence-app/',
+    app_page : 'https://princemerluza.github.io/purecloud-spotify-presence-app/spotify-app.html'
+}
